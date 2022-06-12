d0c0fe9ffbee61f70164bd584f66336f583af8dc
Putifinalian <38107155+Putifinalian@users.noreply.github.com>
2022-06-12 23:49:34 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,6 @@
+<div id="content" class="content">
+	<ol class="breadcrumb float-xl-right">
+		<li class="breadcrumb-item"><a href="<?php echo base_url('home');?>">Dashboard</a></li>
+	</ol>
+	<h1 class="page-header mb-3">Dashboard</h1>
+</div>
