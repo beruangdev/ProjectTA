0ba74d359d9fafba3fc325db0a52c0bb161fc3dc
Putifinalian <38107155+Putifinalian@users.noreply.github.com>
2022-06-13 20:46:46 +0700

    Update Kriteria_model.php

--- Kriteria_model.php
+++ Kriteria_model.php
@@ -3,6 +3,7 @@ class Kriteria_model extends CI_Model{
     
 	public function getAll()
 	{
-		$this->db->get('kriteria')->result();
+		$data = $this->db->query("SELECT * from kriteria");
+		return $data->result();
 	}
 }
